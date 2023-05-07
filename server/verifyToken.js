5e48aa0a94afa65731478e61d01e594d1d54258d
Dhruv Sharma <dhruvamsh@gmail.com>
2023-05-07 10:27:12 +0530

    log

--- server/verifyToken.js
+++ server/verifyToken.js
@@ -3,6 +3,7 @@ import { createError } from "./error.js";
  
 export const verifyToken=(req,res,next)=>{
     const token=req.headers.authorization.replace("Bearer ","");
+    console.log(token)
     if(!token) return next(createError(401,"You are not authenticated"))
 
  
